462615c38b4d324f459771464d239fb943d877d6
Taylor Hart <82608807+thart003@users.noreply.github.com>
2024-10-18 07:00:10 -0700

    Update udf_example.py

--- udf_example.py
+++ udf_example.py
@@ -6,4 +6,22 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import lit, col, udf
 from pyspark.sql.types import IntegerType
 
+spark = (SparkSession.builder
+        .getOrCreate())
+args = getResolvedOptions(sys.argv, ["JOB_NAME", "ds", 'output_table'])
+run_date = args['ds']
+output_table = args['output_table']
+glueContext = GlueContext(spark.sparkContext)
+spark = glueContext.spark_session
+
+df = spark.sql("SELECT * FROM taylorhart.nba_players WHERE surrent_season = '2002')
+
+
+
+
+
+
+
+
+
 
