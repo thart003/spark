ad7e1ceec6b95d4878c11affec138792c462d82b
Taylor Hart <82608807+thart003@users.noreply.github.com>
2024-10-18 06:57:08 -0700

    Update udf_example.py

--- udf_example.py
+++ udf_example.py
@@ -1,1 +1,9 @@
+import sys
+from awsglue.utils import getResolvedOptions
+from awsglue.context import GlueContext
+from aws.glue.job import Job
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import lit, col, udf
+from pyspark.sql.types import IntegerType
+
 
