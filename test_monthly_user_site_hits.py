75a9289e4e575d91bd8a898b8603d679733813ec
Taylor Hart <82608807+thart003@users.noreply.github.com>
2024-10-20 09:56:28 -0700

    Update test_monthly_user_site_hits.py

--- test_monthly_user_site_hits.py
+++ test_monthly_user_site_hits.py
@@ -1,1 +1,21 @@
+from chispa.dataframe_comparer import *
+
+from ..jobs.monthly_user_site_hits.
+from collections import namedtuple
+
+MonthlySiteHit = namedtuple("Monthly
+MonthlySiteHitsAgg = namedtuple("Monthly
+
+def test_monthly_site_hits(spark):
+  ds = "2023-03-01"
+  new_month_start = "2023-04-01"
+  input_data = [
+      MonthlySiteHit(
+          month_start=ds,
+          hit_array=[0,1,3],
+          date_partitions=ds
+      ),
+      MonthlySiteHit(
+          month_start=ds,
+          
 
